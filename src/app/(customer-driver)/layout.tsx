265dab9f0087d9639ba14b43eef174d2a8413f76
Rizal Anggoro <gnoogler4@gmail.com>
2024-10-24 19:17:36 +0700

    fix: base layout for customer and driver

--- src/app/(customer-driver)/layout.tsx
+++ src/app/(customer-driver)/layout.tsx
@@ -6,12 +6,14 @@ export default function Layout(props: PropsWithChildren) {
   return (
     <>
       <div className="bg-muted h-screen w-full overflow-hidden">
-        <div className="max-w-[512px] mx-auto bg-background h-screen shadow flex flex-col">
+        <div className="max-w-[512px] mx-auto bg-background h-screen shadow relative">
           {/* top navbar */}
           <NavbarComponent />
 
           {/* content */}
-          <div className="flex-1 overflow-auto">{props.children}</div>
+          <div className="h-[calc(100vh-8rem)] overflow-y-auto my-16">
+            {props.children}
+          </div>
 
           {/* bottom navbar */}
           <BottomNavbarComponent />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export default function NavbarComponent() {
   return (
     <>
-      <div className="h-16 w-full flex items-center px-4 border-b justify-between">
+      <div className="h-16 w-full flex items-center px-4 border-b justify-between fixed top-0 bg-background max-w-[512px] z-20">
         <Button variant={"link"} className="px-0 font-semibold text-base">
           Unitip
         </Button>
