5b248faa0b0ee1ec667cbee81493b3a540836d82
Rizal Anggoro <gnoogler4@gmail.com>
2024-10-24 19:30:48 +0700

    chore(auth): background

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout(props: PropsWithChildren) {
   return (
     <>
       <div className="bg-muted h-screen w-full overflow-hidden">
-        <div className="max-w-[512px] mx-auto px-4 overflow-y-auto">
+        <div className="max-w-[512px] mx-auto px-4 overflow-y-auto bg-background shadow h-screen">
           {props.children}
         </div>
       </div>
