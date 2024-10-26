87f0f2c291f5e398d3306db950984583b4c18b31
Rizal Anggoro <gnoogler4@gmail.com>
2024-10-26 23:12:16 +0700

    feat: test only: create room chat with other customers

--- src/app/(customer-driver)/chats/page.tsx
+++ src/app/(customer-driver)/chats/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import ButtonCreateChat from "./button-create-chat";
 import ListCustomers from "./list-customers";
 
 export const metadata: Metadata = {
@@ -9,13 +10,17 @@ export default function Page() {
   return (
     <>
       <div>
-        <div className="px-4 pt-4">
-          <p className="text-lg font-semibold">Percakapan</p>
-          <p className="text-muted-foreground text-sm">
-            Berikut daftar beberapa percakapan antara Anda dan driver yang
-            sedang berlangsung. Percakapan ini bersifat pribadi dan akan dihapus
-            ketika orderan telah selesai.
-          </p>
+        <div className="px-4 pt-4 flex items-center gap-2">
+          <div className="flex-1">
+            <p className="text-lg font-semibold">Percakapan</p>
+            <p className="text-muted-foreground text-sm">
+              Berikut daftar beberapa percakapan antara Anda dan driver yang
+              sedang berlangsung. Percakapan ini bersifat pribadi dan akan
+              dihapus ketika orderan telah selesai.
+            </p>
+          </div>
+
+          <ButtonCreateChat />
         </div>
 
         <div className="mt-4">
