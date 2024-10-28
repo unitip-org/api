3973681360f5fcf2e7b1be809d04863b51751563
ReniUswatun <reniuswatunhasanah@student.uns.ac.id>
2024-10-28 21:57:27 +0700

    feat: floating action button, fix: register role token (temporary)

--- src/app/(mobile-first)/(home)/jobs-v2/page.tsx
+++ src/app/(mobile-first)/(home)/jobs-v2/page.tsx
@@ -1,6 +1,21 @@
+import { Button } from "@/components/ui/button";
 import ListJobs from "./list-jobs";
+import { Plus } from "lucide-react";
+import { verifyAuthToken } from "@/lib/auth-token";
+import { cn } from "@/lib/utils";
 
-export default function Page() {
+export default async function Page() {
+  let isAuthenticated = false;
+  let role = "";
+
+  try {
+    const session = await verifyAuthToken();
+    isAuthenticated = true;
+    role = session.role;
+  } catch (e) {
+    isAuthenticated = false;
+  }
+
   return (
     <>
       <div className="pt-20 px-4">
@@ -9,9 +24,21 @@ export default function Page() {
           Berikut beberapa jobs yang ditawarkan oleh mitra atau driver.
         </p>
 
-        <div className="mt-4">
+        <div
+          className={cn(
+            "mt-4",
+            isAuthenticated && role === "driver" ? "mb-[10.5rem]" : "mb-24"
+          )}>
           <ListJobs />
         </div>
+
+        {isAuthenticated && role === "driver" && (
+          <Button
+            className="fixed z-30 bottom-24 right-4 rounded-full size-14"
+            size={"icon"}>
+            <Plus />
+          </Button>
+        )}
       </div>
     </>
   );
