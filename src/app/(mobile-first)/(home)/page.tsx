54279a6db7d4eb13ef37b18c15d1646c798c3449
ReniUswatun <reniuswatunhasanah@student.uns.ac.id>
2024-11-11 19:45:04 +0700

    chore: carousel home page

--- src/app/(mobile-first)/(home)/page.tsx
+++ src/app/(mobile-first)/(home)/page.tsx
@@ -1,15 +1,67 @@
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import TestCounter from "./test-counter";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import React from "react";
 
 export default function Page() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+  const dataImageCarousel = [
+    {
+      id: 1,
+      image: "/images/1.png",
+      alt: "Image1",
+    },
+    {
+      id: 2,
+      image: "/images/2.png",
+      alt: "Image2",
+    },
+    {
+      id: 3,
+      image: "/images/3.png",
+      alt: "Image3",
+    },
+    {
+      id: 4,
+      image: "/images/4.png",
+      alt: "Image4",
+    },
+  ];
   return (
     <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, omnis
-        recusandae magnam, nesciunt vel cumque iure, sed mollitia praesentium
-        enim velit aperiam optio similique! Cumque veniam magnam et optio
-        molestias?
-      </p>
-
+      <div className="mt-16">
+        <div>
+          <Carousel
+            plugins={[plugin.current]}
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            className="w-full">
+            <CarouselContent>
+              {dataImageCarousel.map((images) => (
+                <CarouselItem key={images.id}>
+                  <Image
+                    alt={images.alt}
+                    src={images.image}
+                    objectFit="fill"
+                    width={500}
+                    height={300}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      </div>
       <TestCounter />
     </>
   );
