a518e5984f8a955766ed61803e83f0d0267ae0f5
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-17 09:54:44 +0700

    feat: add role validation to login endpoint and include user ID in response

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -14,6 +14,75 @@ export async function POST(request: Request) {
        * percobaan kedua
        * masukkan user menggunakan role yang diminta.
        */
+      const parsedData = z
+        .object({
+          email: z
+            .string({ required_error: "Alamat email tidak boleh kosong!" })
+            .email("Alamat email tidak valid!"),
+          password: z
+            .string({ required_error: "Kata sandi tidak boleh kosong!" })
+            .min(6, "Kata sandi minimal terdiri dari 6 karakter!"),
+          role: z
+            .string({ required_error: "Role tidak boleh kosong!" })
+            .min(1, "Role tidak boleh kosong!"),
+        })
+        .safeParse({ email, password, role });
+
+      if (!parsedData.success)
+        return APIResponse.BadRequestError(
+          parsedData.error.errors.map((it) => ({
+            message: it.message,
+            path: it.path[0] as string,
+          }))
+        );
+
+      const userQuery = database
+        .selectFrom("users as u")
+        .innerJoin("user_roles as ur", "ur.user", "u.id")
+        .leftJoin("sessions as s", "s.user", "u.id")
+        .select([
+          "u.id",
+          "u.name",
+          "u.email",
+          "u.password",
+          "ur.role",
+          "s.token",
+        ])
+        .where("u.email", "=", email)
+        .where("ur.role", "=", role);
+      const userResult = await userQuery.executeTakeFirst();
+
+      // validasi user account existence dan kata sandi
+      if (
+        !userResult ||
+        (userResult && !(await compare(password, userResult.password)))
+      )
+        return APIResponse.NotFoundError(
+          "Alamat email atau kata sandi tidak valid!"
+        );
+
+      // cek apakah user sedang memiliki session
+      const newToken = generateRandomToken(32);
+      if (userResult.token) {
+        const sessionQuery = database
+          .updateTable("sessions")
+          .where("user", "=", userResult.id as any)
+          .set({
+            token: newToken,
+          })
+          .returning("token");
+        const sessionResult = await sessionQuery.executeTakeFirst();
+
+        if (sessionResult)
+          return APIResponse.Success({
+            need_role: false,
+            roles: [],
+            id: userResult.id,
+            name: userResult.name,
+            email: userResult.email,
+            token: sessionResult,
+          });
+      }
     } else {
       /**
        * percobaan pertama
@@ -92,6 +161,7 @@ export async function POST(request: Request) {
           return APIResponse.Success({
             need_role: false,
             roles: [],
+            id: firstUser.id,
             name: firstUser.name,
             email: firstUser.email,
             token: sessionResult,
