8a35ab87d1a59f64c1b25fda8d89dde68a03f035
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-20 23:58:44 +0700

    feat: add role to user session and token storage in login route

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -79,6 +79,7 @@ export async function POST(request: Request) {
           .where("user", "=", userResult.id as any)
           .set({
             token: newToken,
+            role,
           })
           .returning("token");
         const sessionResult = await sessionQuery.executeTakeFirst();
@@ -98,6 +99,7 @@ export async function POST(request: Request) {
           .insertInto("user_sessions")
           .values({
             token: newToken,
+            role,
             user: userResult.id,
           } as any)
           .returning("token");
