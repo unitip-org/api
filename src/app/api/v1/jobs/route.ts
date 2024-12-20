6ad52eba035213a0c45b49059fa1b15a221fc424
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-20 10:54:35 +0700

    feat: add GET endpoint for retrieving jobs with pagination and customer details

--- src/app/api/v1/jobs/route.ts
+++ src/app/api/v1/jobs/route.ts
@@ -1,6 +1,7 @@
 import { verifyBearerToken } from "@/lib/bearer-token";
 import { database } from "@/lib/database";
 import { APIResponse } from "@/lib/models/api-response";
+import { sql } from "kysely";
 import { NextRequest } from "next/server";
 import { z } from "zod";
 
@@ -84,6 +85,94 @@ export async function POST(request: NextRequest) {
     }
 
     return APIResponse.respondWithServerError();
+  } catch (e) {
+    return APIResponse.respondWithServerError();
+  }
+}
+
+interface JobCustomer {
+  name: string;
+}
+interface Job {
+  id: string;
+  title: string;
+  destination: string;
+  note: string;
+  type: string;
+  pickup_location: string;
+  created_at: string;
+  updated_at: string;
+  customer: JobCustomer;
+}
+
+interface GETResponse {
+  jobs: Job[];
+  page_info: {
+    count: number;
+    page: number;
+    total_pages: number;
+  };
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    // validasi auth token
+    const authorization = await verifyBearerToken(request);
+    if (!authorization) return APIResponse.respondWithUnauthorized();
+
+    // mendapatkan page and limit
+    const searchParams = request.nextUrl.searchParams;
+    const page = Number(searchParams.get("page") || "1");
+    const limit = Number(searchParams.get("limit") || "10");
+
+    const jobsQuery = database
+      .selectFrom("single_jobs as sj")
+      .innerJoin("users as u", "u.id", "sj.customer")
+      .select([
+        "sj.id",
+        "sj.title",
+        "sj.destination",
+        "sj.note",
+        "sj.type",
+        "sj.pickup_location",
+        "u.name as customer_name",
+      ])
+      .select(sql<string>`sj."xata.createdAt"`.as("created_at"))
+      .select(sql<string>`sj."xata.updatedAt"`.as("updated_at"))
+      .offset((page - 1) * limit)
+      .orderBy("created_at", "desc")
+      .limit(limit);
+    const jobsResult = await jobsQuery.execute();
+
+    // mendapatkan total row dari table single jobs
+    const queryCount = database
+      .selectFrom("single_jobs as sj")
+      .select(sql<number>`count(sj.id)`.as("count"));
+    const resultCount = await queryCount.executeTakeFirst();
+
+    return APIResponse.respondWithSuccess<GETResponse>({
+      jobs: jobsResult.map(
+        (it) =>
+          <Job>{
+            id: it.id,
+            title: it.title,
+            destination: it.destination,
+            note: it.note,
+            type: it.type,
+            pickup_location: it.pickup_location,
+            created_at: it.created_at,
+            updated_at: it.updated_at,
+            customer: <JobCustomer>{
+              name: it.customer_name,
+            },
+          }
+      ),
+      page_info: {
+        count: jobsResult.length,
+        page: page,
+        total_pages: Math.ceil((resultCount?.count ?? 0) / limit),
+      },
+    });
   } catch (e) {
     return APIResponse.respondWithServerError();
   }
