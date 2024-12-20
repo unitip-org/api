d19cda148699540c0025c6ad4a62310063ac3741
Rizal Anggoro <gnoogler4@gmail.com>
2024-12-20 19:22:55 +0700

    feat: implement Swagger documentation with security schemes and components

--- src/app/api/v1/docs/page.tsx
+++ src/app/api/v1/docs/page.tsx
@@ -1,39 +1,13 @@
-import docJobs from "@/app/api/v1/jobs/docs.json";
-import { getApiDocs } from "@/lib/swagger";
-import ReactSwagger from "./swagger";
+"use client";
 
-export default async function Page() {
-  const spec = await getApiDocs();
-  const newSpec = {
-    openapi: "3.1.0",
-    info: { title: "Unitip API Documentation", version: "1.0" },
-    components: {
-      securitySchemes: {
-        BearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-      schemas: {
-        hehe: {
-          type: "object",
-          properties: {
-            name: { type: "string" },
-            age: { type: "number" },
-          },
-        },
-      },
-    },
-    paths: {
-      ...docJobs.paths,
-    },
-  };
+import { swaggerSpec } from "@/lib/swagger/spec";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
+export default function Page() {
   return (
-    <section className="container">
-      {/* <ReactSwagger spec={spec} /> */}
-      <ReactSwagger spec={newSpec} />
+    <section className="container mx-auto">
+      <SwaggerUI spec={swaggerSpec} tryItOutEnabled />
     </section>
   );
 }
