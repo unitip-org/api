04c252555826b32d11c589982a02285e8b515237
Retno Pinka <retnopinka20@gmail.com>
2025-01-18 00:20:29 +0700

    feat: create approve applicant by customer

---
+++ src/app/api/v1/jobs/[job_id]/applicants/[applicant_id]/approve/route.ts
@@ -0,0 +1,117 @@
+import { verifyBearerToken } from "@/lib/bearer-token";
+import { database, xata } from "@/lib/database";
+import { APIResponse } from "@/lib/models/api-response";
+
+import { NextRequest } from "next/server";
+import { z } from "zod";
+
+interface POSTResponse {
+  success: boolean;
+  id: string;
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { job_id: string; applicant_id: string } }
+) {
+  try {
+    // const json = await request.json();
+    // const { price } = json;
+    const { job_id: jobId, applicant_id: applicantId } = params;
+
+    // validasi harga dan parameter (job_id dan applicant_id)
+    const data = z
+      .object({
+        // price: z
+        //   .number({ required_error: "Harga tidak boleh kosong!" })
+        //   .min(1, "Harga tidak boleh negatif!"),
+        jobId: z
+          .string({ required_error: "Job ID tidak boleh kosong!" })
+          .min(1, "Job ID tidak boleh kosong!"),
+        applicantId: z
+          .string({ required_error: "Applicant ID tidak boleh kosong!" })
+          .min(1, "Applicant ID tidak boleh kosong!"),
+      })
+      .safeParse({ jobId, applicantId });
+    if (!data.success)
+      return APIResponse.respondWithBadRequest(
+        data.error.errors.map((it) => ({
+          message: it.message,
+          path: it.path[0] as string,
+        }))
+      );
+
+    // validasi token
+    const authorization = await verifyBearerToken(request);
+    if (!authorization) return APIResponse.respondWithUnauthorized;
+
+    // validasi role karena approve hanya bisa dilakukan oleh customer
+    if (authorization.role !== "customer")
+      return APIResponse.respondWithForbidden(
+        "Anda tidak memiliki akses untuk melakukan aksi ini"
+      );
+
+    // mendapatkan semua lamaran dari driver
+    const queryApplicants = database
+      .selectFrom("single_job_applicants as sja")
+      .select(["sja.id", "sja.price", "sja.freelancer"])
+      .where("sja.job", "=", jobId as any);
+    const resultApplicants = await queryApplicants.execute();
+
+    console.log(queryApplicants);
+
+    // ambil lamaran yang sesuai dengan id saat ini
+    const currentApplicant = resultApplicants.find(
+      (it) => it.id === applicantId
+    );
+
+    // validasi jika driver sudah diambil orang
+    if (resultApplicants.length === 0 || !currentApplicant)
+      return APIResponse.respondWithNotFound("Driver sudah ditikung orang!");
+
+    // transaksi untuk delete lamaran dan update table single jobs
+    const result = await xata.transactions.run([
+      // query untuk update
+      {
+        update: {
+          table: "single_jobs",
+          id: jobId,
+          fields: {
+            price: currentApplicant.price,
+            freelancer: currentApplicant.freelancer,
+            status: "ongoing",
+          },
+        },
+      },
+
+      // query untuk delete semua aplicants
+      ...resultApplicants.map(
+        (it) =>
+          ({
+            delete: {
+              table: "single_job_applicants",
+              id: it.id,
+            },
+          } as any)
+      ),
+    ]);
+    console.log(result);
+
+    return APIResponse.respondWithSuccess<POSTResponse>({
+      success: true,
+      id: "dummy",
+    });
+    // validasi jika customer telah approve driver sebelumnya (customer hanya bisa approve satu freelancer)
+
+    // validasi jika freelancer telah diapprove di job lain
+
+    // validasi jika freelancer membatalkan lamaran
+
+    /* gunakan job id, dan applicant id untuk mengambil freelancer id
+    update table jobs untuk mengeset freelancer berdasarkan id (xata transaction)
+    hapus applicants yang lain (xata transaction)*/
+  } catch (e) {
+    console.log(e);
+    return APIResponse.respondWithServerError();
+  }
+}
