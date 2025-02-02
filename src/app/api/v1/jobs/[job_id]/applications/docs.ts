2b68b2fb3ea7cc9fd20652eadf8f4ea53788e049
Rizal Anggoro <gnoogler4@gmail.com>
2025-02-02 19:20:28 +0700

    feat: implement DELETE endpoint for job applications with validation and authorization

--- src/app/api/v1/jobs/[job_id]/applications/docs.ts
+++ src/app/api/v1/jobs/[job_id]/applications/docs.ts
@@ -1,18 +1,20 @@
 import { swaggerSecurity } from "@/lib/swagger/security";
 
+const parameters = [
+  {
+    in: "path",
+    name: "job_id",
+    required: true,
+    schema: { type: "string" },
+  },
+];
+
 export const jobApplicationsByIdPaths = {
   "/api/v1/jobs/{job_id}/applications": {
     patch: {
       tags: ["Jobs"],
       security: swaggerSecurity,
-      parameters: [
-        {
-          in: "path",
-          name: "job_id",
-          required: true,
-          schema: { type: "string" },
-        },
-      ],
+      parameters,
       responses: {
         200: {
           content: {
@@ -28,6 +30,24 @@ export const jobApplicationsByIdPaths = {
         },
       },
     },
-    delete: {},
+    delete: {
+      tags: ["Jobs"],
+      security: swaggerSecurity,
+      parameters,
+      responses: {
+        200: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  id: { type: "string" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 };
--- src/app/api/v1/jobs/[job_id]/applications/route.ts
+++ src/app/api/v1/jobs/[job_id]/applications/route.ts
@@ -66,4 +66,53 @@ export const PATCH = async (request: NextRequest, { params }: Params) => {
   }
 };
 
-export const DELETE = async () => {};
+interface DELETEResponse {
+  id: string;
+}
+export const DELETE = async (request: NextRequest, { params }: Params) => {
+  try {
+    // verifikasi request user
+    const { job_id: jobId } = params;
+    const validate = z
+      .object({
+        jobId: z
+          .string({ required_error: "ID pekerjaan tidak boleh kosong!" })
+          .min(1, "ID pekerjaan tidak boleh kosong!"),
+      })
+      .safeParse({ jobId });
+    if (!validate.success)
+      return APIResponse.respondWithBadRequest(
+        validate.error.errors.map((it) => ({
+          path: it.path[0] as string,
+          message: it.message,
+        }))
+      );
+
+    // verifikasi bearer token
+    const authorization = await verifyBearerToken(request);
+    if (!authorization) return APIResponse.respondWithUnauthorized();
+    const { userId, role } = authorization;
+
+    // verifikasi role user
+    if (role !== "driver")
+      return APIResponse.respondWithForbidden(
+        "Anda tidak memiliki akses untuk melakukan aksi ini!"
+      );
+
+    // cancel job
+    const query = database
+      .updateTable("jobs")
+      .set({ freelancer: null as any })
+      .where("id", "=", jobId)
+      .where("freelancer", "=", userId as any)
+      .returning("id");
+    const result = await query.executeTakeFirstOrThrow();
+
+    return APIResponse.respondWithSuccess<DELETEResponse>({
+      id: result.id,
+    });
+  } catch (e) {
+    console.log(e);
+    return APIResponse.respondWithServerError();
+  }
+};
